806ae14983149a5f65161440e7fb55ac53ec503b
Crazy-Max-2011 <Crazy-Max-2011@yandex.ru>
2025-07-27 18:32:29 +0400

    Update information about dec2places. Add information about Wokwi.

--- src/settings.h
+++ src/settings.h
@@ -1,12 +1,11 @@
 #pragma once
 
-//#define WIFI_SSID "Wokwi-GUEST"
-//#define WIFI_PASS ""
-#define WIFI_SSID "MiHome"
-#define WIFI_PASS "!vlad-02072015!"
+// Для Wokwi: SSID "Wokwi-GUEST", без пароля
+#define WIFI_SSID "Wokwi-GUEST"
+#define WIFI_PASS ""
 
-#define BOT_TOKEN "7761364005:AAHcqdFsU24b-HzXzWFnzcuatlPncFd3wOY"
-#define CHAT_ID "5001027591"
+#define BOT_TOKEN ""
+#define CHAT_ID ""
 
 #define LED_PIN 14
 #define LED_WIDTH 32
